23db59f7079ebf6f29c2c0815ca727346fcf7d82
Ryan Flynn <ryanflynn44@gmail.com>
2020-12-03 17:08:28 -0500

    Installed drei fror orbit controls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useRef, useState} from "react"
 import { Canvas, useFrame } from 'react-three-fiber'
 import { softShadows, MeshWobbleMaterial, OrbitControls} from 'drei'
-
+import { useSpring, a } from 'react-spring/three'
 
 function App() {
   return (
